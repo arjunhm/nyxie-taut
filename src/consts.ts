db37262422cdc3bebc0ecb892397df01b07f27a7
arjunhm <hm.arjun97@gmail.com>
2024-12-18 16:49:16 -0600

    removed lang selector

--- src/consts.ts
+++ src/consts.ts
@@ -35,7 +35,7 @@ export const siteConfig: Config = {
   description: ui[DEFAULT_LANG]["site.description"].text,
   lang: DEFAULT_LANG,
   profile: {
-    author: "Amy Dang",
+    author: "Arjun Handli",
     description: ui[DEFAULT_LANG]["profile.description"].text
   },
   settings: {
